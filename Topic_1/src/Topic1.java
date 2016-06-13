f1e4f268fc168d2ae93ed56b89f37b7cf4dbde57
SimonZhongl <simon.zhongl@hotmail.com>
2016-06-13 23:23:02 +0800

    topic 1

--- Topic_1/src/Topic1.java
+++ Topic_1/src/Topic1.java
@@ -1,6 +1,10 @@
 /**
  * Created by Simon Zhong on 2016/6/13.
+ * Topic 1
+ * 有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
  */
 public class Topic1 {
-
+    public static void main(String [] args){
+        System.out.println("Hello Word!");
+    }
 }
